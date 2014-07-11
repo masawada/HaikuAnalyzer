112bb66be2ba2f5dccd1810ea534aa7e78461b50
Masayoshi Wada <developer@andantesoftware.com>
2014-07-11 15:32:36 +0900

    文節をチェックするように変更した

--- haiku_analyzer.rb
+++ haiku_analyzer.rb
@@ -6,16 +6,23 @@ class HaikuAnalyzer
   end
 
   def parse(text)
-    words = []
+    phrases = []
     sections = []
-    current = 0
     syllabic_sound = [5, 7, 5]
 
-    @nm.parse(text) { |n| words << n.feature.split(",").last unless n.surface.nil? }
+    current = 0
+    @nm.parse(text) do |n|
+      next if n.surface.nil?
+      word_data = n.feature.split(",")
+      current += 1 unless word_data[1].index("助詞") || word_data[1].index("助動詞")
+      phrases[current] = "#{phrases[current]}#{word_data.last}"
+    end
+    phrases.compact!
 
-    words.each do |word|
+    current = 0
+    phrases.each do |phrase|
       return false if syllabic_sound[current].nil?
-      sections[current] = "#{sections[current]}#{word}"
+      sections[current] = "#{sections[current]}#{phrase}"
       current += 1 if sections[current].length >= syllabic_sound[current]
     end
 
